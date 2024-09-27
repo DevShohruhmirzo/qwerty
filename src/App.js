afb2e2225d963de0f0b5e0b343819d431527cf93
DevShohruhmirzo <shohruhmirzonumonjonov@gmail.com>
2024-09-27 17:49:23 +0300

    jhfjgc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar"
+
+
 
 function App() {
+  const name = "Doe"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <h1>My name is {name}</h1>
+      <h2>Hello world</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quia!</p>
     </div>
+    
   );
 }
 
